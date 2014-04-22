a67acedd7fb2cd8a2d5103b6f270cfc6049481a2
Jessica-L <jessica.lynch@colorado.com>
2014-04-22 02:35:34 -0300

    added spec/factories_spec.rb and app/views/users/show.html.erb

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,11 +4,19 @@ describe "User pages" do
 
   subject { page } 
 
+  # NOTE: not sure if need to replace :user with :username
+  # This is cut and pasted from Listing 7.9 from Mike Hartl tutorial
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
   describe "signup page" do
     before { visit '/users/sign_up' }
-
       it { should have_content('Sign up') }
-
     end
 
   describe "signin page" do
